9a12384ece4ba2166bcd586dc0e4b3dfa1a83694
Carlos Sosa <csosa777@gmail.com>
2016-06-06 18:52:07 -0700

    Add files via upload

---
+++ Scripts/onclick_tab.js
@@ -0,0 +1,10 @@
+<script>
+    $('#product-nav').on('click', 'a', function(event) {
+          event.preventDefault();
+
+          var $el = $(event.target).closest('a');
+          var hash = $el.attr('href').split('#')[1];
+          $('a[href="#' + hash + '"]').tab('show');
+        })
+</script> 
+// just add a id to the dropdown menu
